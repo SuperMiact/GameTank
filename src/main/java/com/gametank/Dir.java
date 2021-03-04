0e252577245bcdef7614b104f802eb33a1ddb084
Mawei <506479916@qq.com>
2021-03-04 08:33:38 +0800

    根据坦克方向确定坦克位移

---
+++ src/main/java/com/gametank/Dir.java
@@ -0,0 +1,6 @@
+package com.gametank;
+
+//只能取这四个方向之一
+public enum Dir {
+    LEFT,UP,RIGHT,DOWN
+}
--- src/main/java/com/gametank/TankFrame.java
+++ src/main/java/com/gametank/TankFrame.java
@@ -10,6 +10,8 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     int x = 200, y = 200;
+    Dir dir = Dir.DOWN;
+    private static final int SPEED = 10;
 
     public TankFrame(){
         setSize(800,600);
@@ -29,9 +31,24 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(x , y ,50,50);
-//        x += 10;
-//        y += 10;
+
+        g.fillRect(x,y,50,50);
+
+        switch (dir){
+            case LEFT:
+                x-=SPEED;
+                break;
+            case RIGHT:
+                x+=SPEED;
+                break;
+            case UP:
+                y-=SPEED;
+                break;
+            case DOWN:
+                y+=SPEED;
+                break;
+        }
+
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -64,11 +81,8 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
-
-
-            //x += 200;
-            //重画（刷新），会默认调用 paint
-            //repaint();
+            //改变坦克方向
+            setMainTankDir();
         }
 
         @Override
@@ -93,7 +107,16 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
+
+        private void setMainTankDir(){
+            if(bL) dir = Dir.LEFT;
+            if(bR) dir = Dir.RIGHT;
+            if(bU) dir = Dir.UP;
+            if(bD) dir = Dir.DOWN;
+        }
+
     }
 
 }
