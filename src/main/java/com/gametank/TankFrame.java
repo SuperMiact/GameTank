8b1c5f68cb6acc1d723da4cbf2dd79d6068289b5
Mawei <506479916@qq.com>
2021-03-11 14:14:28 +0800

    new 出五个敌人坦克

--- src/main/java/com/gametank/TankFrame.java
+++ src/main/java/com/gametank/TankFrame.java
@@ -11,8 +11,9 @@ import java.util.List;
 
 public class TankFrame extends Frame {
 
-    Tank myTank = new Tank(200,200,Dir.DOWN,this);
+    Tank myTank = new Tank(200,500,Dir.DOWN,this);
     List<Bullet> bullets = new ArrayList<Bullet>();
+    List<Tank> tanks = new ArrayList<Tank>();
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
     public TankFrame(){
@@ -56,6 +57,9 @@ public class TankFrame extends Frame {
         myTank.paint(g);
         for (int i = 0 ;i<bullets.size();i++){
             bullets.get(i).paint(g);
+        }
+        for (int i = 0 ;i<tanks.size();i++){
+            tanks.get(i).paint(g);
         }
     }
 
