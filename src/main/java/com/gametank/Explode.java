a4927fce9eee5e281a8bba2bac59ad91540e198e
Mawei <506479916@qq.com>
2021-03-11 23:22:00 +0800

    文件追加

---
+++ src/main/java/com/gametank/Explode.java
@@ -0,0 +1,30 @@
+package com.gametank;
+
+import java.awt.*;
+
+public class Explode {
+    public static int WIDTH = ResourceMgr.explodes[0].getWidth();
+    public static int HEIGHT = ResourceMgr.explodes[0].getHeight();
+
+    private int x ,y ;
+
+    private boolean living = true;
+    TankFrame tf = null;
+
+    private int step = 0;
+
+
+    public Explode(int x, int y, TankFrame tf) {
+        this.x = x;
+        this.y = y;
+        this.tf = tf;
+    }
+
+    public void paint(Graphics g){
+        //每paint一次step就加1
+        g.drawImage(ResourceMgr.explodes[step++],x,y,null);
+
+        if (step >= ResourceMgr.explodes.length)
+            step=0;
+    }
+}
